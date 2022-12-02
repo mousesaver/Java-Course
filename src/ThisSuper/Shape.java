30bb17b3ad63263ba1120bcfe3c843910eeeb353
mousesaver <sean.fan87@gmail.com>
2022-12-01 19:21:33 -0800

    This VS Super

---
+++ src/ThisSuper/Shape.java
@@ -0,0 +1,29 @@
+package ThisSuper;
+
+public class Shape {
+    private int x;
+    private int y;
+
+    public Shape() {
+    }
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Square extends Shape {
+    private int width;
+    private int height;
+
+    public Square(int width, int height) {
+        this(0, 0, width, height);
+    }
+
+    public Square(int x, int y, int width, int height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+}
